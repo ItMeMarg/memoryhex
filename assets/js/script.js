41bc6c68b8594edd42701de363222e50875f323f
Margaret Lundgren <margareteliselundgren@gmail.com>
2020-09-20 22:51:03 +0000

    Added hover effects, chained game-pieces js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 //load document before running scripts
 $(document).ready(function() {
-    $('.game-piece').on('click', function() {
-        $(this).css({background:'black'});
+
+    $('.game-piece').on( {
+
+        //game piece obacity higher when over object
+        mouseenter: function() {
+            $(this).animate({opacity:'0.2'});
+        },
+
+        //game piece obacity lower when off object
+        mouseleave: function() {
+            $(this).animate({opacity:'1.0'});
+        },
+
+        //game piece turns black on click
+        click: function() {
+            $(this).css({background:'black'});
+        }
+
     });
+  
 });
