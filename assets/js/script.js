0ae3b101fc9bb6095b075b55ab173cc23cc67ba1
Margaret Lundgren <margareteliselundgren@gmail.com>
2020-09-21 14:35:21 +0000

    Added Fisher-Yates shuffle to color array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,41 @@
 $(document).ready(function() {
 
 //initializations
-let piece = [];
-let colorArray = [
-    "#0F4C81",
-    "#FF6F61",
-    "#5F4B8B",
-    "#88B04B",
-    "#92A8D1",
-    "#F7CAC9",
-    "#955251",
-    "#B163A3",
-    "#009473",
-    "#DD4124",
-    "#D94F70",
-    "#45B5AA"
+var colorArray = [
+    '#0F4C81',
+    '#FF6F61',
+    '#5F4B8B',
+    '#88B04B',
+    '#92A8D1',
+    '#F7CAC9',
+    '#955251',
+    '#B163A3',
+    '#009473',
+    '#DD4124',
+    '#D94F70',
+    '#45B5AA'
 ];
+
+//Fisher-Yates shuffle algorithm 
+let colorShuffle = function(arr) {
+    //tracks place in array
+    let newPos,
+        temp;
+    //iterates backward from 11 until 0
+    for (let i = arr.length - 1; i > 0; i--) {
+        //pseudo random position, rounds to integer
+        newPos = Math.floor(Math.random() * (i + 1));
+        //switching positions
+        temp = arr[i]; 
+        arr[i] = arr [newPos];
+        arr[newPos] = temp; 
+    }
+    return arr;
+};
+//test array shuffle
+let newArray = colorShuffle(colorArray);
+console.log(newArray);
+
 
 //game pieces
 
@@ -32,11 +52,12 @@ let colorArray = [
             $(this).animate({opacity:'1.0'});
         },
 
-        //game piece turns to random color from colorArray on click
+        //game piece turns PINK on click
         click: function() {
-            $(this).css('background', function(i){
-                return colorArray[i % colorArray.length];
-            });
+            $(this).addClass('background', function() {
+
+                
+            }); 
         }
 
     });
