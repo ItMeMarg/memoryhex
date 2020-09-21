2b47d9855d83e4b8b97a71259e9c750b860bc029
Margaret Lundgren <margareteliselundgren@gmail.com>
2020-09-21 00:32:34 +0000

    Added colorArray to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,40 @@
 //load document before running scripts
 $(document).ready(function() {
 
+//initializations
+let piece = [];
+let colorArray = [
+    "#0F4C81",
+    "#FF6F61",
+    "#5F4B8B",
+    "#88B04B",
+    "#92A8D1",
+    "#F7CAC9",
+    "#955251",
+    "#B163A3",
+    "#009473",
+    "#DD4124",
+    "#D94F70",
+    "#45B5AA"
+];
+
+//game pieces
+
     $('.game-piece').on( {
 
-        //game piece obacity higher when over object
+        //game piece opacity higher when over object
         mouseenter: function() {
             $(this).animate({opacity:'0.2'});
         },
 
-        //game piece obacity lower when off object
+        //game piece opacity lower when off object
         mouseleave: function() {
             $(this).animate({opacity:'1.0'});
         },
 
-        //game piece turns black on click
+        //game piece turns PINK on click
         click: function() {
-            $(this).css({background:'black'});
+            $(this).css({background:'pink'});
         }
 
     });
